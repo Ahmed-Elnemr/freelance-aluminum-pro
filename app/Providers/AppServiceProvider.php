320e3d73e67e04b5e502a751c07b48dfbf5b33d2
Ahmed-Elnemr <ahmedelnemr064@gmail.com>
2025-04-13 01:42:52 +0200

    Fix (   )helper echo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,7 @@ class AppServiceProvider extends ServiceProvider
             // Your authorization logic
             return 1;
         });
+
+        Schema::defaultStringLength(191);
     }
 }
