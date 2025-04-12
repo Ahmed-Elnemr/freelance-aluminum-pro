497b338f263955fa8d7135cff5ce334277e2a9ad
Ahmed-Elnemr <ahmedelnemr064@gmail.com>
2025-04-13 01:49:50 +0200

    Fix (   )helper echo

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('email',191)->primary();
             $table->longText('token');
             $table->timestamp('created_at')->nullable();
         });
