6d285a95f802a5bc77bce4561ff80077f2fbce0c
Ahmed-Elnemr <ahmedelnemr064@gmail.com>
2025-04-20 21:03:15 +0200

    Fix (maintenance type)  done

---
+++ database/migrations/2025_04_20_184737_add_service_type_id_to_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreignId('service_type_id')
+                ->nullable()
+                ->constrained('services')
+                ->cascadeOnDelete()->after('service_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign(['service_type_id']);
+            $table->dropColumn('service_type_id');
+        });
+    }
+};
