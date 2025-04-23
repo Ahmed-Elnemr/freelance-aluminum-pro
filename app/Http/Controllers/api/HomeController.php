156dbd1123c27bd919ebcb394272406951a9b5a7
Ahmed-Elnemr <ahmedelnemr064@gmail.com>
2025-04-23 20:22:59 +0200

    Fix (Order) search

--- app/Http/Controllers/api/HomeController.php
+++ app/Http/Controllers/api/HomeController.php
@@ -9,16 +9,29 @@ use App\Http\Resources\ServiceListResource;
 use App\Http\Resources\SliderResource;
 use App\Models\Service;
 use App\Models\Slider;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function home()
+    public function home(Request $request)
     {
-        $slidersInternal=Slider::active()->whereType(SliderTypeEnum::INTERNAL)->get();
-        $allServices=Service::active()->get();
+        $search = $request->get('search') ;
+            $locale = app()->getLocale();
+
+        $slidersInternal = Slider::active()->whereType(SliderTypeEnum::INTERNAL)->get();
+
+        $allServices = Service::active()
+            ->when($search, function ($query) use ($search, $locale) {
+                $query->where(function ($q) use ($search, $locale) {
+                    $q->where("name->{$locale}", 'like', "%{$search}%")
+                        ->orWhere('price', 'like', "%{$search}%");
+                });
+            })
+            ->get();
+
         return ApiResponder::loaded([
-            'sliders'=>SliderResource::collection($slidersInternal),
-            'services'=>ServiceListResource::collection($allServices),
+            'sliders' => SliderResource::collection($slidersInternal),
+            'services' => ServiceListResource::collection($allServices),
         ]);
     }
 }
