4a0dbd06cd3b37f103d4d640aeb0d83fe9c85beb
Ahmed-Elnemr <ahmedelnemr064@gmail.com>
2025-06-11 00:40:47 +0300

    Feat (main Services) search

--- app/Http/Controllers/api/ServiceController.php
+++ app/Http/Controllers/api/ServiceController.php
@@ -139,7 +139,7 @@ class ServiceController extends Controller
                 });
             })
             ->latest()
-            ->get();
+            ->paginate(10);
 
         return ApiResponder::loaded([
             'services' => ServiceListResource::collection($allServices),
